2867306428c02d4c0234e9c1940caf7227509924
victor.dunaev <victor.dunaev@localhost>
2010-05-25 06:43:33 +0000

    ?????? ???? ??????????? ??????

--- whoisalive/wx_Ping.h
+++ whoisalive/wx_Ping.h
@@ -7,32 +7,62 @@
 #include "../common/my_inet.h"
 #include "../common/my_http.h"
 #include "../common/my_thread.h"
+#include "../common/my_mru.h"
+
+#include <memory>
 
 #include <boost/config/warning_disable.hpp> /* против unsafe */
 
 //(*Headers(wx_Ping)
 #include <wx/frame.h>
 class wxPanel;
-class wxStaticBitmap;
 class wxTextCtrl;
 class wxFlexGridSizer;
 //*)
 
+#include <wx/textctrl.h> 
+#include <wx/bitmap.h> 
+
+struct ping_info
+{
+	enum state_t {unknown, reply, timeout};
+	bool is_archive;
+	state_t state;
+	posix_time::ptime start;
+	posix_time::time_duration time;
+};
+
 class wx_Ping: public wxFrame
 {
 private:
+	typedef my::mru::list<unsigned short, ping_info> pings_list;
+
 	bool terminate_;
 	who::server &server_;
 	tcp::socket socket_;
 	ipobject_t *object_;
 	my::http::reply reply_;
 	mutex read_mutex_;
+	pings_list pings_;
+	mutex pings_mutex_;
+	bool archive_mode_;
+	wxTextAttr last_archive_style_;
+	std::wstring last_archive_text_;
+	wxBitmap bitmap_;
+	mutex bitmap_mutex_;
+	int active_index_;
 
 	void handle_read(const boost::system::error_code& error,
 		size_t bytes_transferred);
 
+	void repaint();
+
 	//(*Handlers(wx_Ping)
 	void OnClose(wxCloseEvent& event);
+	void on_pingpanel_paint(wxPaintEvent& event);
+	void on_pingpanel_mousemove(wxMouseEvent& event);
+	void on_pingpanel_erasebackground(wxEraseEvent& event);
+	void on_pingpanel_mouseleave(wxMouseEvent& event);
 	//*)
 
 	DECLARE_EVENT_TABLE()
@@ -42,7 +72,7 @@ protected:
 	//(*Identifiers(wx_Ping)
 	static const long ID_TEXTCTRL2;
 	static const long ID_PANEL1;
-	static const long ID_STATICBITMAP2;
+	static const long ID_PINGPANEL;
 	static const long ID_PINGTEXTCTRL;
 	//*)
 	
@@ -51,7 +81,7 @@ public:
 	virtual ~wx_Ping();
 
 	//(*Declarations(wx_Ping)
-	wxStaticBitmap* m_bitmap;
+	wxPanel* m_pingpanel;
 	wxPanel* Panel1;
 	wxTextCtrl* m_ping_textctrl;
 	wxTextCtrl* TextCtrl2;
